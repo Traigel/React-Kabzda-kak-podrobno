b1cc75ba8c92dd1a83d5ebfb3c8fc7838833bd01
Vova Traihel <vovatraigel@gmail.com>
2022-07-06 17:46:30 +0300

    12 uncontrolled input

---
+++ src/stories/input.stories.tsx
@@ -0,0 +1,32 @@
+import React, {ChangeEvent, useRef, useState} from 'react';
+
+export default {
+    title: 'input',
+    //component: ,
+}
+
+export const UncontrolledInput = () => <input/>;
+
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+    return <>
+        <input onChange={onChangeHandler}/> - {value}
+    </>
+};
+
+export const GetValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+    return <>
+        <input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        - actual value: {value}
+    </>
+};
+
+export const ControlledInputWithFixedValue = () => <input value={'Hello'}/>;
