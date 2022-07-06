f48f0fbe2ae07db164f008fe4fad0b1a5e9dda48
Vova Traihel <vovatraigel@gmail.com>
2022-07-06 18:30:42 +0300

    13 controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -29,4 +29,26 @@ export const GetValueOfUncontrolledInput = () => {
     </>
 };
 
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value)
+    return <input value={parentValue} onChange={onChangeHandler}/>
+};
+
+export const ControlledCheckbox = () => {
+    const [checked, setChecked] = useState(false)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setChecked(e.currentTarget.checked)
+    return <input type={'checkbox'} checked={checked} onChange={onChangeHandler}/>
+};
+
+export const ControlledSelect = () => {
+    const [selectValue, setSelectValue] = useState<string | undefined>('1')
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => setSelectValue(e.currentTarget.value)
+    return <select value={selectValue} onChange={onChangeHandler}>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Kiev</option>
+        <option value={'3'}>Moscow</option>
+    </select>
+};
+
 export const ControlledInputWithFixedValue = () => <input value={'Hello'}/>;
