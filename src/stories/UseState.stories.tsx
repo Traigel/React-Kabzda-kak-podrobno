aadffb42c2fffb045feab8843e04dc1629ed46ca
Vova Traihel <vovatraigel@gmail.com>
2022-08-05 12:30:20 +0300

    20 - pro useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'useState demo'
+}
+
+function generateDate() {
+    console.log('generateDate')
+    return 1
+}
+
+export const Example = () => {
+  console.log('Example')
+    const [counter, setCounter] = useState(generateDate)
+
+    return <>
+        <button
+            onClick={()=> setCounter(state => state + 1)}
+        >+</button>
+        {counter}
+    </>
+}
